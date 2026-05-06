8f83c5015336edb831c9762239f46d534f4b23e1
arachchillagec <arachchillagec@wsc.school.nz>
2026-05-07 11:41:16 +1200

    adding maths part from the classroom to the VS code new lesson (2)

---
+++ Lesson 2/maths.py
@@ -0,0 +1,24 @@
+# Print the answers to the following questions. Get the computer to do the math for you!
+
+# What is 376 + 209 + 44439?
+
+# What is 2345678 - 678 - 1?
+
+# What is 345 divided by 34?
+
+# What is 567 * 34 * 3?
+
+
+# Print 'hello' 32 times.
+
+# -------------------------
+
+# EXTENSION
+
+# What is 2345 / 766 rounded to the nearest whole number?
+
+# What is 456 to the power of 23?
+
+# What is the remainder if you divide 345 by 32?
+
+# --------------------------
