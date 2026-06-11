51104efdb5459726ace437bc33c6cfbd18f965ec
arachchillagec <arachchillagec@wsc.school.nz>
2026-06-11 12:29:38 +1200

    add a lesson
    pokemon

---
+++ Lesson 12/pokemon.py
@@ -0,0 +1,29 @@
+# =====================================================================
+# PROJECT: Pokemon
+# Create a battle program where you battle a random pokemon
+# =====================================================================
+
+# TODO Import random module
+import random
+# Wild Pokemon
+# TODO Create a multidimensional list that holds 4 pokemon names and their max health (you choose)
+wild_pokemon = [
+    {"name" : "Pikachu", "health" : 1000},
+    ["lucky",10],
+    ["Lucario",111],
+    ["Mew",6767]
+]
+# User Pokemon
+# TODO Create a multidimensional list that holds 4 pokemon attacks and their different damage
+
+# TODO Create a variable to hold a randomised wild pokemon
+# TODO Create a current_health variable and set it to the max health of the random pokemon
+# TODO Tell the user what pokemon they're facing
+# TODO Create a while loop that continues until current health <= 0
+    # TODO Ask the user which attack they'd like to use (list all 4 options, numbered); save input
+    # TODO Use try except to ensure the user has input a number; if they didn't tell them so and then use 'continue' to restart the loop
+    # TODO Using the number, get the attack damage value and minus it from current health
+
+# TODO Tell the user they defeated the pokemon
+
+RAND_POKEMON ["health"]
