402fbae35c4718d67530244d3635fd688050fe84
arachchillagec <arachchillagec@wsc.school.nz>
2026-06-25 11:25:16 +1200

    adding and making the quiz more smooth working

---
+++ quiz.py
@@ -0,0 +1,67 @@
+
+
+print("""""
+ ██████╗ ██╗   ██╗██╗███████╗
+██╔═══██╗██║   ██║██║╚══███╔╝
+██║   ██║██║   ██║██║  ███╔╝
+██║▄▄ ██║██║   ██║██║ ███╔╝
+╚██████╔╝╚██████╔╝██║███████╗
+ ╚═════╝  ╚═════╝ ╚═╝╚══════╝
+""")
+
+print("Are you ready to start the Quiz")
+input("if your ready press enter")
+
+# The First Question
+
+print("Question 01;")
+print("What is the best car brand in the world. ")
+guess = input("your answer : ")
+if guess =="Toyota":
+   print("correct answer ")
+else:
+   print("wrong answer and the answer is Toyota")
+
+# The second Question
+
+print("Question 02")
+print("What is the most expensive shoe in the world?")
+guess = input("your answer : ")
+if guess == "Moon Star Shoe":
+   print("your answer is correct")
+else:
+   print("wrong answer the answer is Moon Star Shoes by Antonio Vietri (20 million)")
+
+
+print("Question 03;")
+print("What is the name of our planet? ")
+guess = input("your answer : ")
+if guess == "Earth":
+   print("correct answer ")
+else:
+   print("Your answer was wrong and the answer is Earth")
+
+print("Question 04;")
+print(" Which planet is closest to the sun? ")
+guess = input("your answer : ")
+if guess == "Mercury":
+   print("correct answer  ")
+else:
+   print("Your answer was wrong and the answer is Mercury")
+
+print("Question 05;")
+print("How many bones do we have in an ear? (number only)")
+guess = input("your answer : ")
+if guess == "3":
+   print("correct answer is 3 ")
+else:
+   print("Your answer wrong it's (3)")
+   
+   print("thank you to play my crazy game")
+   print("See Ya!!!!!!!!!!")
+
+
+
+
+
+
